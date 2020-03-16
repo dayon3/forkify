4eb5e258d1ba285a5047d7ab68b2986c2ddfab89
dayon3 <aliasdayone@gmail.com>
2020-03-16 20:03:19 +0300

    AJAX loading spinner + format title

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,9 +7,10 @@ export default class Search {
 
 	async getResults() {
 		const key = 'd5f0dbcef7914b76b5454c29d273e4a3';
+		const number = 30;
 		try {
 			const res = await axios(
-				`https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${this.query}`
+				`https://api.spoonacular.com/recipes/search?apiKey=${key}&query=${this.query}&number=${number}`
 			);
 			this.result = res.data.results;
 			//console.log(this.result);
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,16 +10,45 @@ export const clearResults = () => {
 	elements.searchResList.innerHTML = '';
 };
 
+/**
+ * 'Pasta with tomato and spinach'
+ * acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+ * acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+ * acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+ * acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+ * acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato']
+ */
+
+const limitRecipeTitle = (title, limit = 17) => {
+	const newTitle = [];
+	if (title.length > limit) {
+		title.split(' ').reduce((accumulator, current) => {
+			if (accumulator + current.length <= limit) {
+				newTitle.push(current);
+			}
+			return accumulator + current.length;
+		}, 0);
+
+		// return result
+		return `${newTitle.join(' ')} ...`;
+	}
+	return title;
+};
+
 const renderRecipe = recipe => {
 	const markup = `
     <li>
       <a class="results__link" href="${recipe.id}">
         <figure class="results__fig">
-          <img src="https://spoonacular.com/recipeImages/${recipe.id}-312x231.jpg" alt="Test">
+          <img src="https://spoonacular.com/recipeImages/${
+						recipe.id
+					}-312x231.jpg" alt="Test">
         </figure>
         <div class="results__data">
-          <h4 class="results__name">${recipe.title}</h4>
-          <p class="results__author">Ready in ${recipe.readyInMinutes} minutes.</p>
+          <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+          <p class="results__author">Ready in ${
+						recipe.readyInMinutes
+					} minutes.</p>
         </div>
       </a>
     </li>
